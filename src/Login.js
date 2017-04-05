cda876e44cee2c0664ab53d1768d1b772b1c6dc9
Evgen <e.salyakin@gmail.com>
2017-04-05 22:40:05 +0300

    Zlata create login page

---
+++ src/Login.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <main className="login">
+                <aside>
+                    <h1>Water chat</h1>
+                    <p className="description">-  online speaking chat with game "Five in line"</p>
+                    <h3>By
+                        <a href="https://github.com/ZlataSon">Zlata Son</a>
+                        <span> & </span>
+                        <a href="https://github.com/EvgenSalyakin">Evgen S.</a>
+                    </h3>
+
+                    <div className="login-form">
+                        <input type="text" placeholder="Enter your nickname"/>
+
+                        {/*<input type="text" placeholder="Enter your age"/>*/}
+
+                        <div id="mainselection">
+                            <select>
+                                <option>&#9794; Male or &#9792; Female</option>
+                                <option>&#9794; Male</option>
+                                <option>&#9792; Female</option>
+                            </select>
+                        </div>
+
+                        <button>Log In</button>
+                    </div>
+
+                </aside>
+                <section> </section>
+            </main>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from './App';
-import Home from './Home';
+import Login from './Login';
 import Chat from './Chat';
 import Game from './Game';
 
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={Home} />
+            <IndexRoute component={Login} />
             <Route path="/chat" component={Chat} />
             <Route path="/game" component={Game} />
         </Route>
